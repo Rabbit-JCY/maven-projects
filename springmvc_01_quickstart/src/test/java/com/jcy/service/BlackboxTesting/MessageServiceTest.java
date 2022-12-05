1ea47a06fa1e5aaa8a1ec9d7bcdfc4e39a825289
rabbit <928042317@qq.com>
2022-12-05 14:44:13 +0000

    Initial commit

--- springmvc_01_quickstart/src/test/java/com/jcy/service/BlackboxTesting/MessageServiceTest.java
+++ springmvc_01_quickstart/src/test/java/com/jcy/service/BlackboxTesting/MessageServiceTest.java
@@ -1,4 +1,11 @@
 package com.jcy.service.BlackboxTesting;
 
+import com.jcy.config.SpringConfig;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = SpringConfig.class)
 public class MessageServiceTest {
 }
